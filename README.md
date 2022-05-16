4fca3475b28a19382778bc93846eaa3485060d78
Borislav Grebenar <boki.grebenar@gmail.com>
2022-05-16 15:51:10 +0200

    16.05.2022, iadd static analisys target, readme, minor fixes

--- README.md
+++ README.md
@@ -15,12 +15,30 @@ RunTime cache has been created using ordinary stl hash map, but in real\
 use, of course, some more cache friendly datastructure, should be used,\
 anyway, the idea and way of thinkig is what is important here.\
 Regarding autodiff, I used reverse approach with autodiffs var type, but I tried
-forward dual type as well for first derivate.\
+forward dual type as well, only for first derivate, but I had to make some
+template gymnastic to make it work in compile time using the same alphaelem function, but I resolved that\
+using alpha_elemptr explicit instantiation(now not used, but commented out, and present)\
+to specify overload for autodiff to choose for runtime calculations.\
 Main issue for me here was to acomplish compile time calculations, in order to\
-finish that I used gcem as 3pty lib for compiletime mathematics, as an example.\
+finish that, I used gcem as 3pty lib for compiletime mathematics, as an example.\
 Regarding tests, I made simple test, w/o using any particular framework, but
 I can do that if needed with my prefered catch2 with cmake integration,for example, or any other.\
-C++20 standard is used and compiled with gcc12 with -fno-bultins as wel as woth Visual studio 2022.
+C++20 standard is used and compiled with gcc12 with -fno-bultins as wel as woth Visual studio 2022.\
+I compiled with asan and ubsan and runing it, experiencing no issues.\
+As a solution, alpha finction has signature:\
+void alpha(decimal_type T, std::span<const decimal_type> Tc,\
+           std::span<const decimal_type> omega, std::size_t H,\
+           ResultDataSet result)\
+where is\n
+using ResultSet = std::array<decimal_type, No>;
+using ResultDataSet = std::span<ResultSet>;
+\
+Also clang-tidy is used as static analizer, and t is ingreted with cmake as
+target for static analisys, one can run it with\
+make -C ./build/out clang-tidy-check. (for linux, of course)\
+Tidy complains with "no str() member in iostream", but source construction should
+work with c++20. All other warning and complains comming from 3pty.\
+\
 Finally, you can find screenshot below, with result of executed srk binary for\
 task 1 and 2.\
 Task3 is still work in progres, and I will pushed it soon.\
--- main_ad.cpp
+++ main_ad.cpp
@@ -1,12 +1,12 @@
+#include <algorithm>
 #include <autodiff/reverse/var.hpp>
 #include <gcem/gcem.hpp>
 #include <iomanip>
 #include <iostream>
 #include <iterator>
 #include <span>
-#include <algorithm>
+#include <sstream>
 #include <unordered_map>
-
 using namespace autodiff;
 
 template<typename Type>
@@ -17,8 +17,8 @@ constexpr std::size_t HIndex = 0;
 //compile time cache size
 constexpr std::size_t CTCR = 10;
 
-constexpr std::array<std::size_t, 4> precisions = {6,5,7,10};
-constexpr std::array<const char*, No> elements = {"Hydrogen","Nitrogen","Oxygen"};
+constexpr std::array<std::size_t, 4> precisions = {6, 5, 7, 10};
+constexpr std::array<const char*, No> elements = {"Hydrogen", "Nitrogen", "Oxygen"};
 using decimal_type = double;
 constexpr decimal_type None(std::numeric_limits<double>::quiet_NaN());
 constexpr decimal_type c1(0.48508), c2(1.55174), c3(0.1561), c1H(1.202), c2H(-0.30228);
@@ -31,9 +31,9 @@ auto rtcrs = std::unordered_map<decimal_type, std::array<decimal_type, tcs.size(
 template<typename Ttype>
 constexpr auto alpha_H(const Ttype T, const std::size_t H)
 {
-    if constexpr (numeric_type<decltype(T)>)
-    if(std::is_constant_evaluated())
-        return c1H * gcem::exp(c2H * (T / tcs[H]));
+    if constexpr(numeric_type<decltype(T)>)
+        if(std::is_constant_evaluated())
+            return c1H * gcem::exp(c2H * (T / tcs[H]));
     return c1H * exp(c2H * (T / tcs[H]));
 }
 
@@ -41,11 +41,11 @@ constexpr auto alpha_H(const Ttype T, const std::size_t H)
 template<typename Ttype, typename TcType, typename OmegaType>
 constexpr auto alpha_elem(const Ttype T, const TcType Tc, const OmegaType omega) -> std::decay_t<decltype(T)>
 {
-    if constexpr (numeric_type<decltype(T)>)
-    if(std::is_constant_evaluated()) {
-        decltype(T) r = 1 + (c1 + c2 * omega - c3 * omega * omega) * (1 - gcem::sqrt(T / Tc));
-        return r * r;
-    }
+    if constexpr(numeric_type<decltype(T)>)
+        if(std::is_constant_evaluated()) {
+            decltype(T) r = 1 + (c1 + c2 * omega - c3 * omega * omega) * (1 - gcem::sqrt(T / Tc));
+            return r * r;
+        }
     decltype(T) r = 1 + (c1 + c2 * omega - c3 * omega * omega) * (1 - sqrt(T / Tc));
     return r * r;
 }
@@ -89,21 +89,22 @@ auto calculateVars(var T, std::span<const decimal_type> Tc, std::span<const deci
 template<typename C, typename Ttype>
 void printData(const C& c, const Ttype T, const std::size_t order)
 {
- auto tstr = (std::ostringstream()<<std::fixed<<std::setprecision(2)<<T).str();
- if(!order){
-        std::cout<<std::setw(32)<<std::left<<"Task 1 and 2"<<std::setw(1)<<'|'<<std::setw(15);
-        std::ranges::for_each(elements,[](const auto& el){std::cout<<std::right<<std::setw(15)<<el<<'|';});
-        std::cout<<'\n';
+    auto tstr = (std::ostringstream() << std::fixed << std::setprecision(2) << T).str();
+    if(!order) {
+        std::cout << std::setw(32) << std::left << "Task 1 and 2" << std::setw(1) << '|' << std::setw(15);
+        std::ranges::for_each(elements, [](const auto& el) { std::cout << std::right << std::setw(15) << el << '|'; });
+        std::cout << '\n';
     }
     auto p{precisions[order]};
-    auto message = order?
-        std::string("d").append(std::to_string(order)).append("\\alpha/").append("dT"):std::string("alpha values");
-        std::cout <<std::setw(32)<< std::fixed <<std::setprecision(2)<< std::left << std::string("T(").append(tstr).append(") ").append(message);
-	auto out = std::ostream_iterator<typename C::value_type>(std::cout,"|");
-        std::cout <<std::setw(1)<< std::left << std::fixed<<'|';
-    if(p<8)std::cout<< std::setprecision(p);
-    else std::cout<<std::scientific;
-    std::ranges::for_each(c,[&out](const auto& el){std::cout<<std::setw(15)<<std::right;*out=el;});
+    auto message = order ? std::string("d").append(std::to_string(order)).append("\\alpha/").append("dT") : std::string("alpha values");
+    std::cout << std::setw(32) << std::fixed << std::setprecision(2) << std::left << std::string("T(").append(tstr).append(") ").append(message);
+    auto out = std::ostream_iterator<typename C::value_type>(std::cout, "|");
+    std::cout << std::setw(1) << std::left << std::fixed << '|';
+    if(p < 8)
+        std::cout << std::setprecision(p);
+    else
+        std::cout << std::scientific;
+    std::ranges::for_each(c, [&out](const auto& el) {std::cout<<std::setw(15)<<std::right;*out=el; });
     std::cout << '\n';
 }
 
@@ -114,7 +115,8 @@ void alpha(decimal_type T, std::span<const decimal_type> Tc,
            ResultDataSet result)
 {
     const auto resultDataSetSize = result.size();
-    if (resultDataSetSize == 0) return;
+    if(resultDataSetSize == 0)
+        return;
     auto resultSetIter = result.begin();
     // first, search precalculated cache ctcrs for Temperature
     bool found(false);
@@ -122,69 +124,73 @@ void alpha(decimal_type T, std::span<const decimal_type> Tc,
         if(const auto& [temp, alphas] = *ctcrs_iter; temp == T) {
             //populate result
             std::ranges::copy(alphas, resultSetIter->begin());
-            auto tstr = (std::ostringstream()<<std::fixed<<std::setprecision(2)<<T).str();
-            std::cout << "results for values for T("<<tstr<<") found in CompileTime cache\n";
+            auto tstr = (std::ostringstream() << std::fixed << std::setprecision(2) << T).str();
+            std::cout << "results for values for T(" << tstr << ") found in CompileTime cache\n";
             found = true;
         }
     }
     if(false == found) {
-    // now, search runtime cache  rtcrs for Temperature
-    // finally calculate coeficients, if we had no cached data
-    // and populate runtime cache
-    auto ValPairIter = rtcrs.try_emplace(T, calculateAlphas(T, H));
-    if(!ValPairIter.second)
-        std::cout << "results found in RunTimeCache\n";
-    else
-        std::cout << "new calculation of result set\n";
-    //populate result
-    std::ranges::copy(ValPairIter.first->second, resultSetIter->begin());
+        // now, search runtime cache  rtcrs for Temperature
+        // finally calculate coeficients, if we had no cached data
+        // and populate runtime cache
+        auto ValPairIter = rtcrs.try_emplace(T, calculateAlphas(T, H));
+        if(!ValPairIter.second)
+            std::cout << "results found in RunTimeCache\n";
+        else
+            std::cout << "new calculation of result set\n";
+        //populate result
+        std::ranges::copy(ValPairIter.first->second, resultSetIter->begin());
     }
-    if (resultDataSetSize == 1) return;
+    if(resultDataSetSize == 1)
+        return;
     ++resultSetIter;
     var Tt = T;
-    auto rtelem = calculateVars(Tt,Tc,omega,H);
+    auto rtelem = calculateVars(Tt, Tc, omega, H);
     auto rtelem1 = decltype(rtelem){};
     auto resultSetIndex(0U);
     auto resultSetElemIter = resultSetIter->begin();
-    for(auto& elem : rtelem){
+    for(auto& elem : rtelem) {
         rtelem1[resultSetIndex] = derivativesx(elem, wrt(Tt))[0];
         *(resultSetElemIter++) = val(rtelem1[resultSetIndex++]);
     }
-    if (resultDataSetSize == 2) return;
+    if(resultDataSetSize == 2)
+        return;
     ++resultSetIter;
     auto rtelem2 = decltype(rtelem1){};
     resultSetElemIter = resultSetIter->begin();
     resultSetIndex = 0U;
-    for(auto& elem : rtelem1){
+    for(auto& elem : rtelem1) {
         rtelem2[resultSetIndex] = derivativesx(elem, wrt(Tt))[0];
         *(resultSetElemIter++) = val(rtelem2[resultSetIndex++]);
     }
-    if (resultDataSetSize == 3) return;
+    if(resultDataSetSize == 3)
+        return;
     ++resultSetIter;
     auto rtelem3 = decltype(rtelem2){};
     resultSetElemIter = resultSetIter->begin();
     resultSetIndex = 0U;
-    for(auto& elem : rtelem2){
+    for(auto& elem : rtelem2) {
         rtelem3[resultSetIndex] = derivativesx(elem, wrt(Tt))[0];
         *(resultSetElemIter++) = val(rtelem3[resultSetIndex++]);
     }
-    if (resultDataSetSize == 4) return;
+    if(resultDataSetSize == 4)
+        return;
     ++resultSetIter;
     resultSetElemIter = resultSetIter->begin();
-    for(auto& elem : rtelem3){
+    for(auto& elem : rtelem3) {
         *(resultSetElemIter++) = val(derivativesx(elem, wrt(Tt))[0]);
     }
-
 }
 
-auto test () {
+auto test()
+{
     //-------------------------------
-    std::cout<<("*** simple T E S T ***\n\n");
+    std::cout << ("*** simple T E S T ***\n\n");
     std::cout << "content of compile time calculated cache:\n";
     for(const auto& [T, alphas] : ctcrs) {
         if(std::isnan(T))
             break;
-        printData(alphas,T, 0);
+        printData(alphas, T, 0);
     }
 
     std::cout << "test compile time calculation\n";
@@ -194,9 +200,9 @@ auto test () {
     std::cout << "ctH = " << ctH << '\n';
     std::cout << "ctN = " << ctN << '\n';
     auto rtH = alpha_H(cT, 0);
-    var T=300;
+    var T = 300;
     var rtN = alpha_elem(T, tcs[1], omegas[1]);
-    auto alpha_elemptr = alpha_elem<var, decimal_type, decimal_type>;
+    //auto alpha_elemptr = alpha_elem<var, decimal_type, decimal_type>;
     //double dudT = derivative(alpha_elemptr, wrt(T), at(T, tcs[1], omegas[1]));
     //auto dudT = derivativesx(alpha_elemptr, wrt(T));
     auto [dudT] = derivativesx(rtN, wrt(T));
@@ -206,11 +212,11 @@ auto test () {
     std::cout << "rtN = " << rtN << '\n';
     std::cout << "(T=300,N) d\\alpha/dT = " << dudT << '\n';
     //-----------------------------------
-    std::cout <<("*** END T E S T ***\n\n");
+    std::cout << ("*** END T E S T ***\n\n");
 }
 auto main(int argc, char** argv) -> int
 {
-    if(argc==2 && std::string(argv[1])=="test"){
+    if(argc == 2 && std::string(argv[1]) == "test") {
         test();
         return 0;
     }
@@ -218,11 +224,10 @@ auto main(int argc, char** argv) -> int
     auto T = decimal_type(300);
     alpha(T, tcs, omegas, 0, result);
     auto order = 0U;
-    for(const auto& resultSet:result) {
+    for(const auto& resultSet : result) {
         printData(resultSet, T, order++);
     }
     std::cout << '\n';
 
     return 0;
 }
-
